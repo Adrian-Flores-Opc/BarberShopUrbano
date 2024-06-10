44f7179b1b65dc92be6ac7e20a7f240ac6d3a142
Adrian Condori Flores <adriflores987@gmail.com>
2024-06-10 07:53:20 -0400

    footer

--- src/app/components/customers/footer/footer.component.ts
+++ src/app/components/customers/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatCardModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
