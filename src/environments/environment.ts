bc8286501d172adabd6ecceb9c7ca43b9c0ade17
Adrian Condori Flores <adriflores987@gmail.com>
2024-08-10 10:18:44 -0400

    login integracion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiEndPointAuth: 'https://localhost:50121/',
-    apiEndPoint: 'https://localhost:50121/',
+    // apiEndPointAuth: 'https://localhost:50121/',
+    apiEndPointAuth: 'https://devtnw00/TEST/',
+    apiEndPoint: 'https://devtnw00/TEST/',
     apiEndPointToken: ''
 }
