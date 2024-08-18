8fd9079ad2a508248a1210e7162340f2c9b66fff
Adrian Condori Flores <adriflores987@gmail.com>
2024-08-18 11:18:51 -0400

    integracion

--- src/app/components/customers/landing/services-main/services-main.component.ts
+++ src/app/components/customers/landing/services-main/services-main.component.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAccordion } from "@angular/material/expansion";
 
 @Component({
   selector: 'app-services-main',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatExpansionModule],
+  imports: [MatCardModule, MatButtonModule, MatExpansionModule, MatAccordion],
   templateUrl: './services-main.component.html',
   styleUrl: './services-main.component.scss'
 })
 export class ServicesMainComponent {
+  // @ViewChild(MatAccordion) accordion: MatAccordion;
+
   readonly panelOpenState = signal(false);
+  // public isTabOpen: Boolean = false;
+  // public tabType: string = "";
+  // clickOntab(tab) {
+  //   console.log("tab", tab)
+  //   this.isTabOpen = !this.isTabOpen;
+  //   this.tabType = tab;
+  // }
 }
