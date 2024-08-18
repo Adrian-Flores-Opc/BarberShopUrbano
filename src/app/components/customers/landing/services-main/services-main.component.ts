c91aeb6df2a7251252e79ca784afb7351c99d399
Adrian Condori Flores <adriflores987@gmail.com>
2024-08-18 11:01:28 -0400

    integracion collapse

--- src/app/components/customers/landing/services-main/services-main.component.ts
+++ src/app/components/customers/landing/services-main/services-main.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-services-main',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatExpansionModule],
   templateUrl: './services-main.component.html',
   styleUrl: './services-main.component.scss'
 })
 export class ServicesMainComponent {
-
+  readonly panelOpenState = signal(false);
 }
