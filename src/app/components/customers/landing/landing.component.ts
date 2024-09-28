ed30b04d54c241b91b02ef17eeec797cf001e8c1
Adrian Condori Flores <adriflores987@gmail.com>
2024-09-28 13:13:24 -0400

    Integracion

--- src/app/components/customers/landing/landing.component.ts
+++ src/app/components/customers/landing/landing.component.ts
@@ -5,13 +5,14 @@ import { CarouselMainComponent } from './carousel-main/carousel-main.component';
 import { InformationMainComponent } from './information-main/information-main.component';
 import { ServicesMainComponent } from './services-main/services-main.component';
 import { ArtistsMainComponent } from './artists-main/artists-main.component';
+import { CarouselMinimalMainComponent } from './carousel-minimal-main/carousel-minimal-main.component';
 
 
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, CarouselMainComponent, InformationMainComponent, ServicesMainComponent, ArtistsMainComponent],
+  imports: [MatCardModule, MatButtonModule, CarouselMainComponent, InformationMainComponent, ServicesMainComponent, ArtistsMainComponent, CarouselMinimalMainComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
