54ae37329363c6e5d7f65e25bdc802b5b3387520
Adrian Condori Flores <adriflores987@gmail.com>
2024-09-29 16:29:59 -0400

    add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // apiEndPointAuth: 'https://localhost:50121/',
-    apiEndPointAuth: 'https://devtnw00/TEST/',
-    apiEndPoint: 'https://devtnw00/TEST/',
+    // apiEndPointAuth: 'https://devtnw00/TEST/',
+    apiEndPointAuth: 'https://localhost:50121/',
+    apiEndPoint: 'https://localhost:50121/',
     apiEndPointToken: ''
 }
