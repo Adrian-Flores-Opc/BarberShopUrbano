ae1da7194df68572c5bd9aef39cd50e1ca8688ac
Yrvin Perez Choque <YPerezC@bcp.com.bo>
2024-10-18 23:35:21 -0400

    cambios yrvin

--- src/app/components/customers/landing/carousel-minimal-main/carousel-minimal-main.component.ts
+++ src/app/components/customers/landing/carousel-minimal-main/carousel-minimal-main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, OnInit, PLATFORM_ID, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-minimal-main',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
   templateUrl: './carousel-minimal-main.component.html',
   styleUrl: './carousel-minimal-main.component.scss'
 })
-export class CarouselMinimalMainComponent {
+export class CarouselMinimalMainComponent implements OnInit {
+  @ViewChild('carouselTrack', { static: true }) carouselTrack!: ElementRef;
+  index = 0;
+  intervalId: any;
 
+  constructor(private renderer: Renderer2, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.startCarousel();
+    }
+  }
+
+  startCarousel(): void {
+    const track = this.carouselTrack.nativeElement;
+    const images = track.querySelectorAll('img');
+    const totalImages = images.length / 2; // Solo la mitad son imágenes únicas
+    const imageWidth = images[0].clientWidth;
+
+    this.intervalId = setInterval(() => {
+      this.index++;
+      if (this.index >= totalImages) {
+        this.index = 0;
+        this.renderer.setStyle(track, 'transition', 'none');
+        this.renderer.setStyle(track, 'transform', `translateX(0)`);
+        setTimeout(() => {
+          this.renderer.setStyle(track, 'transition', 'transform 0.5s ease-in-out');
+          this.index++;
+          this.renderer.setStyle(track, 'transform', `translateX(-${this.index * imageWidth}px)`);
+        }, 50);
+      } else {
+        this.renderer.setStyle(track, 'transform', `translateX(-${this.index * imageWidth}px)`);
+      }
+    }, 5000); // Cambia cada 5 segundos
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
